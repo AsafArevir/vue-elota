fbbeb220f87bf8d1df2a814d568347971828bdb4
AsafArevir <asafdiaz7@gmail.com>
2023-07-13 11:58:18 -0600

    360 Image new

--- src/views/ImageView.vue
+++ src/views/ImageView.vue
@@ -1,8 +1,11 @@
 <template>
 	<a-scene>
 		<a-assets>
-    			<img id="sky" src="../assets/img/imagenTD.jpg">
-  		</a-assets>
-	  	<a-sky src="#sky"></a-sky>
+			<img id="sky" src="../assets/img/imagenTD.jpg">
+			<img id="homeThumbnail" src="../assets/img/return.png">
+		</a-assets>
+		<a-sky src="#sky"></a-sky>
+		<a-entity link=" <router-link to='/'></router-link>; title: My Homepage; image: #homeThumbnail"></a-entity>
+
 	</a-scene>
 </template>
